26a73c00f031c9da56ed5f8add519a4c12545df3
fandytcc <fandytsui@gmail.com>
2017-12-29 11:05:11 +0100

    create songs

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -2,6 +2,36 @@ function toggleSelected() {
   $(this).parent().parent().toggleClass("warning");
 }
 
+function nextSongId(){
+  return $(".song").length + 1;
+}
+
+function createSong(title){
+
+  var checkboxId = "song-" + nextSongId();
+
+  var label = $("<label></label>")
+    .attr("for", checkboxId)
+    .html(title);
+
+  var checkbox = $("<input type="checkbox" value="1" />")
+    .attr("id", checkboxId)
+    .bind("change", toggleSelected);
+
+  var tableRow = $("<tr class="song"></tr>")
+    .append($("<td>").append(checkbox))
+    .append($("<td>").append(label));
+
+  $("#songList").append(tableRow);
+}
+
+function submitSong(event){
+  event.preventDefault();
+  var title = $("song_title").val();
+  createSong(title);
+  $("#song_title").val(null);
+}
+
 function removeSong(event){
   event.preventDefault();
   $(".warning").remove();
@@ -9,5 +39,6 @@ function removeSong(event){
 
 $(document).ready(function() {
   $("input[type=checkbox]").bind("change", toggleSelected);
+  $("form").bind("submit", submitSong);
   $("#delete").bind("click", removeSong);
 });
